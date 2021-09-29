0b4fd0b2fe022f76b3cbe14fcbf57d83423ec269
Muhammad Awais <awaisshaikh94@gmail.com>
2021-09-29 17:14:05 +0500

    listing part api integration done

--- src/app/core/http/services/users/users.service.ts
+++ src/app/core/http/services/users/users.service.ts
@@ -1,12 +1,22 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { ConfigService } from '../../config/config.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
+  url: string = `${this.config.apiEndpoint}/api/users`;
 
   constructor(
+    private config: ConfigService,
     private http: HttpClient
   ) { }
+
+  getUsers(page: number) {
+    const params = new HttpParams()
+      .set('page', page)
+
+    return this.http.get<any>(this.url, {params: params});
+  }
 }
--- src/app/modules/users/listing/listing.component.ts
+++ src/app/modules/users/listing/listing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from 'src/app/core/http/services/users/users.service';
 
 @Component({
   selector: 'app-listing',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listing.component.scss']
 })
 export class ListingComponent implements OnInit {
+  users: any = [];
+  data: any = {};
+  page: number = 1;
 
-  constructor() { }
+  constructor(
+    private usersService: UsersService
+  ) { }
 
   ngOnInit(): void {
+    this.getUserListing();
+  }
+
+  getUserListing() {
+    this.usersService.getUsers(2).subscribe((res: any) => {
+      this.data = res;
+      this.users = res.data;
+      console.log(this.users);
+      
+    });
   }
 
 }
