ff89b9e189c12e27e1b6dfefd6f5bb4dc5f5d727
Muhammad Awais <awaisshaikh94@gmail.com>
2021-09-29 16:39:15 +0500

    setting up angular http in the project

--- src/app/modules/users/users.module.ts
+++ src/app/modules/users/users.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersComponent } from './users.component';
@@ -15,7 +16,8 @@ import { ListingComponent } from './listing/listing.component';
   ],
   imports: [
     CommonModule,
-    UsersRoutingModule
+    UsersRoutingModule,
+    HttpClientModule
   ]
 })
 export class UsersModule { }
